8ab5278157e73c8a64a94e56648081c55c921c51
malik <Malik-Al>
2021-12-09 22:20:43 +0600

    add Action creators. Работа с массивами. Рефакторинг

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
 import './App.css';
 import {useDispatch, useSelector} from "react-redux";
+import {addCustomerAction, removeCustomerAction} from "./store/customerReducer";
+import {addCashAction, getCashAction} from "./store/cashReducer";
 
 function App() {
   const dispatch = useDispatch()
   const cash = useSelector(state => state.cash.cash)
+  const customers = useSelector(state => state.customers.customers)
+
+
   console.log(cash)
 
   const addCash = (cash) => {
-    return dispatch({type: 'ADD_CASH', payload: cash})
+    return dispatch(addCashAction(cash))
   }
 
   const getCash = (cash) => {
-    return dispatch({type: 'GET_CASH', payload: cash})
+    return dispatch(getCashAction(cash))
+  }
+
+  const addCustomers = (name) => {
+    const customers = {
+      name,
+      id: Date.now()
+    }
+    dispatch(addCustomerAction(customers))
+  }
+
+  const removeCustomer = (customer) => {
+    return dispatch(removeCustomerAction(customer.id))
   }
 
   return (
     <div className='app'>
+      <div style={{fontSize:'3rem'}}>{cash}</div>
+
       <div style={{display: 'flex'}}>
-        <div style={{fontSize:'3rem'}}>{cash}</div>
         <button onClick={() => addCash(Number(prompt()))}>Пополнить счет</button>
         <button onClick={() => getCash(Number(prompt()))}>Снять со счета</button>
       </div>
+
+      <div style={{display: 'flex', margin: '10px'}}>
+        <button onClick={() => addCustomers(prompt())}>Добавить клиента</button>
+        <button onClick={() => removeCustomer()}>Убрать клиента</button>
+      </div>
+
+        {customers.length > 0 ?
+            <div>
+              {customers.map((customer, id) =>
+                <div
+                    onClick={() => removeCustomer(customer)}
+                    key={id}
+                    style={{fontSize: '2rem', border: '1px solid black', padding: '10px', marginTop:5}}
+                >
+                  {customer.name}
+                </div>
+              )}
+            </div>
+            :
+            <div>
+              Клиенты отсутствуют!
+            </div>
+        }
     </div>
   );
 }
