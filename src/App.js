0bc1df3e74831bac016023f87004c7f26ef23856
malik <Malik-Al>
2021-12-10 01:34:33 +0600

    add Action creators. Redux thunk и асинхронные действия

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {useDispatch, useSelector} from "react-redux";
 import {addCustomerAction, removeCustomerAction} from "./store/customerReducer";
 import {addCashAction, getCashAction} from "./store/cashReducer";
+import {fetchCustomers} from "./asyncAction/customers";
 
 function App() {
   const dispatch = useDispatch()
@@ -43,6 +44,8 @@ function App() {
       <div style={{display: 'flex', margin: '10px'}}>
         <button onClick={() => addCustomers(prompt())}>Добавить клиента</button>
         <button onClick={() => removeCustomer()}>Убрать клиента</button>
+        <button onClick={() => dispatch(fetchCustomers())}>Получить клиентов из базы</button>
+
       </div>
 
         {customers.length > 0 ?
@@ -51,7 +54,7 @@ function App() {
                 <div
                     onClick={() => removeCustomer(customer)}
                     key={id}
-                    style={{fontSize: '2rem', border: '1px solid black', padding: '10px', marginTop:5}}
+                    style={{fontSize: '2rem', border: '1px solid black', padding: '5px', marginTop:5}}
                 >
                   {customer.name}
                 </div>
